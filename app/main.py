665a4ec885c120c4dcfcf52664d91f65b7b517ac
Denis <deonis_sys@outlook.com>
2026-06-08 15:59:26 +0400

    fix: add logging to lifespan startup

--- app/main.py
+++ app/main.py
@@ -2,11 +2,16 @@ from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from app.api.routes import health, prices
 from app.db.database import create_tables
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    logger.info("Creating tables...")
     await create_tables()
+    logger.info("Tables created!")
     yield
 
 
