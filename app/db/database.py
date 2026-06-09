e4b9d641546a321750ce5e958be7e5fd7cc7dfc8
Denis <deonis_sys@outlook.com>
2026-06-09 16:28:05 +0400

    refactor: replace os.getenv with pydantic-settings in database config

--- app/db/database.py
+++ app/db/database.py
@@ -1,15 +1,9 @@
-from dotenv import load_dotenv
 from app.models.coin import Base
-import os
+from app.core.config import settings
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine, AsyncSession
 
 
-load_dotenv()
-
-DATABASE_URL = os.getenv("DATABASE_URL")
-assert DATABASE_URL, "DATABASE_URL is not set in .env"
-
-engine = create_async_engine(DATABASE_URL)
+engine = create_async_engine(settings.DATABASE_URL)
 
 SessionLocal = async_sessionmaker(
     bind=engine,
